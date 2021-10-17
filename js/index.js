ef153087a1d34015cb84beee7c1575e5c6b51af3
Anastasia <nastyae08@mail.ru>
2021-10-18 00:32:47 +1000

    обавили интерактивный таймер с js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+   //Tabs
+
    const tabs = document.querySelectorAll('.tabheader__item'),
          tabsContent = document.querySelectorAll('.tabcontent'),
          tabsParent = document.querySelector('.tabheader__items');
@@ -22,30 +24,74 @@ document.addEventListener('DOMContentLoaded', () => {
    hideTabContent();                                        
    showTabContent();
 
-   tabsParent.addEventListener('click', function(event) {             //делегирование от tabsParent к tabs
+   tabsParent.addEventListener('click', function(event) {             //делегирование от tabsParent к tabs по клику
      const target = event.target;
-      if(target && target.classList.contains('tabheader__item')) {
-         tabs.forEach((item, i) => {
-            if (target == item) {
-               hideTabContent();
-               showTabContent(i);
+      if(target && target.classList.contains('tabheader__item')) {   //если у эл-тов tabsParent есть класс .tabheader__item
+         tabs.forEach((item, i) => {                                 //то перебираем элементы tabs
+            if (target == item) {                                    //если эл-т tabs == эл-ту tabsParent
+               hideTabContent();                                     //скрываем все, что было
+               showTabContent(i);                                    //ставим в эктив нужный элемент
             }
          });
       }
    });
 
+   // Timer
 
+   const deadline = '2021-12-31';                                    //вводим еобходимое нам время
 
+   function getTimeRemaining(endtime) {                              //функция рассчитывющая сколько осталось до deadline
+      const t = Date.parse(endtime) - Date.parse(new Date()),        // total - итог
+         days = Math.floor( (t/(1000*60*60*24)) ),                   //рассчет месяцы, дни, часы, минуты
+         seconds = Math.floor( (t/1000) % 60 ),
+         minutes = Math.floor( (t/1000/60) % 60 ),
+         hours = Math.floor( (t/(1000*60*60) % 24) );
 
+      return {                                                       //возвращается объект со всеми данными
+         'total': t,
+         'days': days,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds
+      };
+   }
 
+   function getZero(num){                                           //проверяем на наличие нулей
+      if (num >= 0 && num < 10) { 
+         return '0' + num;
+      } else {
+         return num;
+      }
+   }
 
+   function setClock(selector, endtime) {                          //само взаимодействие со страницей
 
+      const timer = document.querySelector(selector),              //забираем с html нужные значения по селектору
+            days = timer.querySelector("#days"),                     
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);             //обновляем таймер каждую секунду
 
+      updateClock();                                                //вызываем следующую функцию, чтобы при обновлении стр не было задержек
 
+      function updateClock() {
+         const t = getTimeRemaining(endtime);                       //вызываем первую функию, идет рассчет
 
+         days.innerHTML = getZero(t.days);                          //вставляем в html код полученные значения
+         hours.innerHTML = getZero(t.hours);
+         minutes.innerHTML = getZero(t.minutes);
+         seconds.innerHTML = getZero(t.seconds);
 
+         if (t.total <= 0) {                                        //при достижении 000 - останавливаем таймер
+               clearInterval(timeInterval);
+         }
+      }
+   }
 
+   setClock('.timer', deadline);                                   //вызываем функцию
 
+   
 
 
 });
