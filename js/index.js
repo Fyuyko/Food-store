794c2cd0b9424655e5f75f8fb1d8b8e5bad441ba
Anastasia <nastyae08@mail.ru>
2021-10-22 04:06:50 +1000

    добавили слайдер

--- js/index.js
+++ js/index.js
@@ -437,12 +437,206 @@ document.addEventListener('DOMContentLoaded', () => {
    
 
 
-   //Slider
-   const slider = document.querySelectorAll('.offer__slide'),
-         prev = 
+   /* //Slider вариант #1
+   const slides = document.querySelectorAll('.offer__slide'),   //забираем слайды
+         prev = document.querySelector('.offer__slider-prev'),  //стрелка предыдущий
+         next = document.querySelector('.offer__slider-next'),  //стрелка следующий
+         total = document.querySelector('#total'), //номер след
+         current = document.querySelector('#current'); //номер этого слайда
+   let slideIndex = 1; //проверка индекса слайда
+
+   showSlides(slideIndex);              //чтобы изначально показывало
+
+   if (slides.length < 10) {                    //номер последнего слайда
+      total.textContent = `0${slides.length}`;
+   } else {
+      total.textContent = slides.length;
+   }
+
+
+   function showSlides(n) {            //показ слайдов
+      if (n > slides.length) {         //если №слайда больше кол-ва слайдов, опять 1
+         slideIndex = 1;
+      }
+
+      if (n < 1) {                     //если №слайда меньше 1 - № последнего слайда
+         slideIndex = slides.length;
+      }
+
+      slides.forEach(item => item.style.display = 'none');  //пока скрываем все слайды
+
+      slides[slideIndex - 1].style.display = 'block';       //показываем только нужный нам
+
+      if (slides.length < 10) {                            //номер текущего слайда
+         current.textContent = `0${slideIndex}`;
+      } else {
+         current.textContent = slideIndex;
+      }
+   }
+
+   function plusSlides(n) {                                //+1 к n после верхней функции, которая здесь же вызывется
+      showSlides(slideIndex += n);
+   }
+
+   prev.addEventListener('click', () => {                  //обр событий на кнопочки
+      plusSlides(-1);
+   });
+
+   next.addEventListener('click', () => {
+      plusSlides(1);
+   }); */
+
+   // Slider вариант #2 (№62)
+              //доп в html слайдер оборачиваем в див
+   let offset = 0;     //отступ вправо-лево с пом трансформ
+   let slideIndex = 1;
+
+   const slides = document.querySelectorAll('.offer__slide'), //описание выше
+         slider = document.querySelector('.offer__slider'),   //весь слайдер
+         prev = document.querySelector('.offer__slider-prev'),
+         next = document.querySelector('.offer__slider-next'),
+         total = document.querySelector('#total'),
+         current = document.querySelector('#current'),
+         slidesWrapper = document.querySelector('.offer__slider-wrapper'), //обертка
+         width = window.getComputedStyle(slidesWrapper).width, //ширина обертки - забираем с css
+         slidesField = document.querySelector('.offer__slider-inner'); //внутри обертки див
+
+   if (slides.length < 10) {                   //описание выше
+       total.textContent = `0${slides.length}`;
+       current.textContent =  `0${slideIndex}`;
+   } else {
+       total.textContent = slides.length;
+       current.textContent =  slideIndex;
+   }
+   
+   slidesField.style.width = 100 * slides.length + '%'; //ширину задаем равную 100*кол-во слайдов в %
+   slidesField.style.display = 'flex';          //чтобы высроились слайды в шнрнгу
+   slidesField.style.transition = '0.5s all';   //плавное передвижение
+
+   slidesWrapper.style.overflow = 'hidden';     //скрываем элементы не попадающие в область видимости
+
+   slides.forEach(slide => {
+       slide.style.width = width; //устанавливаем одинаков ширину для всех слайдов
+   });
+
+
+   //навигация
+   slider.style.position = 'relative'; //чтобы абсолютно позиционировать
+
+    const indicators = document.createElement('ol'),  //создаем список
+          dots = [];         //создаем массив
+    indicators.classList.add('carousel-indicators');  //добавляем класс
+    indicators.style.cssText = `  
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `; // Если хотите - добавьте в стили, но иногда у нас нет доступа к стилям
+    slider.append(indicators);    //помещаем обертку в слайдер
+
+    for (let i = 0; i < slides.length; i++) {       //создаем нужное кол-во точек
+        const dot = document.createElement('li');  //создаем элемент списка
+        dot.setAttribute('data-slide-to', i + 1);  //опр атрибут для указания №
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;  //доб стили
+        if (i == 0) {            //если активный эл-т меняем опасити, в 2р больше
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);  //добавляем значения, были активными
+    }
 
 
 
+   next.addEventListener('click', () => {
+      if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+         offset = 0;          //если дошли до конца, возвр в начало
+      } else {
+         offset += +width.slice(0, width.length - 2); //если нет смещаемся дальше
+      }
 
+      slidesField.style.transform = `translateX(-${offset}px)`; //для сдвига в лево в пх
+
+      if (slideIndex == slides.length) {      //для номера слайда
+         slideIndex = 1;                    //до конца-перейти на 1
+      } else {
+         slideIndex++;                      //не последнй - +1
+      }
+
+      if (slides.length < 10) {      //опять проверяем на нужность 0
+         current.textContent =  `0${slideIndex}`;  
+      } else {
+         current.textContent =  slideIndex;
+      }
+
+      dots.forEach(dot => dot.style.opacity = ".5"); //изначально опасити 0,5
+      dots[slideIndex-1].style.opacity = 1;          //активна - опасити 1
+   });
+
+   prev.addEventListener('click', () => {  //для сдвига в право
+      if (offset == 0) {                  //если первый слайд
+         offset = +width.slice(0, width.length - 2) * (slides.length - 1); //смещ к последнему
+      } else {
+         offset -= +width.slice(0, width.length - 2); //иначе на 1 сл назад
+      }
+
+      slidesField.style.transform = `translateX(-${offset}px)`;//для сдвига
+      
+      
+      if (slideIndex == 1) {//как и в next все
+
+         slideIndex = slides.length;
+      } else {
+         slideIndex--;
+      }
+
+      if (slides.length < 10) {
+         current.textContent =  `0${slideIndex}`;
+      } else {
+         current.textContent =  slideIndex;
+      }
+
+      dots.forEach(dot => dot.style.opacity = ".5");  //как для next
+      dots[slideIndex-1].style.opacity = 1;
+   });
+
+   dots.forEach(dot => {        //чтобы при клике на точку-переходить на нее
+      dot.addEventListener('click', (e) => {   //при клике на точку
+          const slideTo = e.target.getAttribute('data-slide-to'); //тот самый атрибут
+
+          slideIndex = slideTo;     //индекс равен выбранной точке
+          offset = +width.slice(0, width.length - 2) * (slideTo - 1); //запис ширину
+
+          slidesField.style.transform = `translateX(-${offset}px)`;  //запис перемещение
+
+          if (slides.length < 10) {                    //проверяем на 0
+              current.textContent =  `0${slideIndex}`;
+          } else {
+              current.textContent =  slideIndex;
+          }
+
+          dots.forEach(dot => dot.style.opacity = ".5");
+          dots[slideIndex-1].style.opacity = 1;
+      });
+  });
    
 });
